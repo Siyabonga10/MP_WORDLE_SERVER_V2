6133c2f6f5eff2e46c9ff7f936bfc1e8d4eca452
Siya10 <leyanesiyabonga00@gmail.com>
2025-07-20 18:01:10 +0200

    feat: Database context done

---
+++ Data/GameDb.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MP_WORDLE_SERVER_V2.Models;
+
+namespace MP_WORDLE_SERVER_V2.Data
+{
+    public class GameDb(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GamePlayerJunction> GamePlayerJunctions { get; set; }
+    }
+}
