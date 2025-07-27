93c256ca2cc7041f9a20d447a53fcd4754b4a692
Siya10 <leyanesiyabonga00@gmail.com>
2025-07-27 10:56:02 +0200

    chore: just another migration to support the updated player models

--- Migrations/20250727085419_AddedPlayerHash.Designer.cs
+++ Migrations/20250727085419_AddedPlayerHash.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MP_WORDLE_SERVER_V2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MP_WORDLE_SERVER_V2.Migrations
 {
     [DbContext(typeof(GameDb))]
-    partial class GameDbModelSnapshot : ModelSnapshot
+    [Migration("20250727085419_AddedPlayerHash")]
+    partial class AddedPlayerHash
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.7");
---
+++ Migrations/20250727085419_AddedPlayerHash.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MP_WORDLE_SERVER_V2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPlayerHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Players",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Players");
+        }
+    }
+}
