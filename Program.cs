7d7e8692fcd4be9eeb91ef6688c1ac8e879bbc8c
Siya10 <leyanesiyabonga00@gmail.com>
2025-07-30 09:25:21 +0200

    wip: azure keyvault

--- Program.cs
+++ Program.cs
@@ -22,6 +22,9 @@ if (!builder.Environment.IsDevelopment())
     }
 }
 
+if (builder.Configuration.GetConnectionString("DefaultConnection") == null)
+    throw new("Could not load database connection string");
+
 builder.Services.AddDbContextFactory<GameCache>(
     options => options.UseInMemoryDatabase("temp"),
     ServiceLifetime.Singleton
